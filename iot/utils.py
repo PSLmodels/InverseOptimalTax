378a793042e2d88798f05cab1cb95e18208267ba
jdebacker <jason.debacker@gmail.com>
2021-09-24 22:25:50 -0400

    add module of utilities

---
+++ iot/utils.py
@@ -0,0 +1,20 @@
+
+
+def wavg(group, avg_name, weight_name):
+    '''
+    Computes weighted averages by group.
+
+    Args:
+        group (Pandas DataFrame): data for the particular grouping
+        avg_name (string): name of variable to compute wgt avg with
+        weight_name (string): name of weighting variables
+
+    Returns:
+        d (scalar): weighted avg for the group
+    '''
+    d = group[avg_name]
+    w = group[weight_name]
+    try:
+        return (d * w).sum() / w.sum()
+    except Warning:
+        return d.mean()
