544adf9d29e1f4ea093596a83ef754bcc454b966
jdebacker <jason.debacker@gmail.com>
2021-09-26 23:25:31 -0400

    try diff black formatting

--- iot/inverse_optimal_tax.py
+++ iot/inverse_optimal_tax.py
@@ -200,10 +200,7 @@ class IOT:
         g_z = (
             1
             + ((self.theta_z * self.inc_elast * self.mtr) / (1 - self.mtr))
-            + (
-                (self.inc_elast * self.z * self.mtr_prime)
-                / (1 - self.mtr) ** 2
-            )
-        )
+            + ((self.inc_elast * self.z * self.mtr_prime) / (1 - self.mtr) ** 2)
+         )
 
         return g_z
