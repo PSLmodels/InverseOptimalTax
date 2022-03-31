d6cf19a004a58a2305894421ce500354d509d7a9
jdebacker <jason.debacker@gmail.com>
2022-03-31 10:06:07 -0400

    create module to store constants used

---
+++ iot/constants.py
@@ -0,0 +1,9 @@
+CURRENT_YEAR = 2022
+
+OUTPUT_LABELS = {
+    "e00200p": "Wages and Salaries, Primary Filer",
+    "e00200": "Wages and Salaries",
+    "expanded_income": "Expanded Income",
+    "z": "$z$", "g_z": "$g_z$", "f": "$$f(z)$$", "f_prime": "$$f'(z)$$",
+    "theta_z": "$$\\theta_z$$", "mtr": "$T'(z)$", "mtr_prime": "$$T''(z)$$"
+}
